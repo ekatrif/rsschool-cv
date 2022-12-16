e8862027d3ae39d39ac19bd67656b9cc64514cca
ekatrif <katiapervak@gmail.com>
2022-12-16 12:32:33 +0100

    feat: add code examples

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function toggleHiddenClass() {
+    let codeExamples = document.querySelector(".code-examples");
+    codeExamples.addEventListener("click",function(e) {
+        if (e.target.classList.contains("code-item__title")) {
+            let code=e.target.parentElement.querySelector(".code-item__code");
+            console.log(code);
+            code.classList.toggle("hidden");
+        }
+    })
+    }
+window.onload=toggleHiddenClass();
